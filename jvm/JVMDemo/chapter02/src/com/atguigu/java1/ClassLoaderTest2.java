750068305f6e5a682d1c71ef80d550f11986ef83
EXsYang <yangda27@aliyun.com>
2024-06-07 22:24:06 +0800

    commit

--- jvm/JVMDemo/chapter02/src/com/atguigu/java1/ClassLoaderTest2.java
+++ jvm/JVMDemo/chapter02/src/com/atguigu/java1/ClassLoaderTest2.java
@@ -9,14 +9,22 @@ public class ClassLoaderTest2 {
         try {
             //1.
             ClassLoader classLoader = Class.forName("java.lang.String").getClassLoader();
-            System.out.println(classLoader);
+            System.out.println(classLoader);//null
             //2.
             ClassLoader classLoader1 = Thread.currentThread().getContextClassLoader();
-            System.out.println(classLoader1);
+            System.out.println(classLoader1);//sun.misc.Launcher$AppClassLoader@18b4aac2
+
+
+
+
 
             //3.
-            ClassLoader classLoader2 = ClassLoader.getSystemClassLoader().getParent();
-            System.out.println(classLoader2);
+            ClassLoader classLoader2 = ClassLoader.getSystemClassLoader();
+            System.out.println(classLoader2);//sun.misc.Launcher$AppClassLoader@18b4aac2
+
+            //4.
+            ClassLoader classLoader3 = ClassLoader.getSystemClassLoader().getParent();
+            System.out.println(classLoader3);//sun.misc.Launcher$ExtClassLoader@1b6d3586
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
--- jvm/JVMDemo/chapter05/src/com/atguigu/java/StackErrorTest.java
+++ jvm/JVMDemo/chapter05/src/com/atguigu/java/StackErrorTest.java
@@ -7,6 +7,31 @@ package com.atguigu.java;
  *
  *  默认情况下：count : 11420
  *  设置栈的大小： -Xss256k : count : 2465
+ *
+ */
+
+/**
+ * https://docs.oracle.com/en/java/javase/11/tools/java.html#GUID-3B1CE181-CD30-4178-9602-230B800D4FAE
+ * 官方文档中对-Xss的解读
+ * -Xss size
+ * Sets the thread stack size (in bytes). Append the letter k or K to indicate KB, m or M to indicate MB, and g or G to indicate GB. The default value depends on the platform:
+ *
+ * Linux/x64 (64-bit): 1024 KB
+ *
+ * macOS (64-bit): 1024 KB
+ *
+ * Oracle Solaris/x64 (64-bit): 1024 KB
+ *
+ * Windows: The default value depends on virtual memory 【在Windows系统下线程栈的大小取决于虚拟机内存】
+ *
+ * The following examples set the thread stack size to 1024 KB in different units:
+ *
+ *
+ * Copy
+ * -Xss1m
+ * -Xss1024k
+ * -Xss1048576
+ * This option is similar to -XX:ThreadStackSize.
  */
 public class StackErrorTest {
     private static int count = 1;
