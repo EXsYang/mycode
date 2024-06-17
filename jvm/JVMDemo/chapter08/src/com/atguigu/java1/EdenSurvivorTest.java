b930d7ba6665b7b2984448541ca8854bb41ef78d
EXsYang <yangda27@aliyun.com>
2024-06-17 21:30:55 +0800

    jvm code commit

--- jvm/JVMDemo/chapter08/src/com/atguigu/java1/EdenSurvivorTest.java
+++ jvm/JVMDemo/chapter08/src/com/atguigu/java1/EdenSurvivorTest.java
@@ -4,11 +4,27 @@ package com.atguigu.java1;
  * -Xms600m -Xmx600m
  *
  * -XX:NewRatio ： 设置新生代与老年代的比例。默认值是2.
- * -XX:SurvivorRatio ：设置新生代中Eden区与Survivor区的比例。默认值是8
+ * -XX:SurvivorRatio ：设置新生代中Eden区与Survivor区的比例。默认值是8【实际测试是6】
  * -XX:-UseAdaptiveSizePolicy ：关闭自适应的内存分配策略  （暂时用不到）
  * -Xmn:设置新生代的空间的大小。 （一般不设置）
  *
- * @author shkstart  shkstart@126.com
+ * 如果同时设置了-XX:NewRatio和-Xmn, 则以显示的设置新生代的内存大小的 -Xmn为准
+ * 测试参数如下:
+ * -Xms600m -Xmx600m (默认不设置-XX:NewRatio时,该参数默认值就为2)
+ * -Xms600m -Xmx600m -Xmn100m
+ *
+ *
+ *  存在的问题:
+ *  默认情况下 新生代中Eden:Survivor0:Survivor1 为 8 : 1 : 1
+ *  但是实际发现 新生代中Eden:Survivor0:Survivor1 为 6 : 1 : 1
+ *  要想8 : 1 : 1 需要显示的设置一下JVM参数 -XX:SurvivorRatio=8
+ *  才会变为8 : 1 : 1
+ *
+ *
+ * 设置参数:
+ * -Xms600m -Xmx600m -XX:SurvivorRatio=8
+ * -Xms600m -Xmx600m -Xmn100m
+ *
  * @create 2020  17:23
  */
 public class EdenSurvivorTest {
--- jvm/JVMDemo/chapter08/src/com/atguigu/java2/ScalarReplace.java
+++ jvm/JVMDemo/chapter08/src/com/atguigu/java2/ScalarReplace.java
@@ -1,8 +1,13 @@
 package com.atguigu.java2;
 
 /**
- * 标量替换测试
+ * 标量替换测试。
+ * 标量替换是被应用起来了，但逃逸分析还不成熟，并没有被应用起来。
+ * 这里测试的逃逸分析带来的优化提升，主要来源于标量替换
+ *
  *  -Xmx100m -Xms100m -XX:+DoEscapeAnalysis -XX:+PrintGC -XX:-EliminateAllocations
+ *
+ * 标量替换的参数 -XX:-EliminateAllocations  【默认是打开的 -XX:+EliminateAllocations】
  * @author shkstart  shkstart@126.com
  * @create 2020  12:01
  */
@@ -25,6 +30,15 @@ public class ScalarReplace {
         }
         long end = System.currentTimeMillis();
         System.out.println("花费的时间为： " + (end - start) + " ms");
+
+
+        // 为了方便查看标量替换参数是否默认打开的，线程sleep
+        // try {
+        //     Thread.sleep(1000000);
+        // } catch (InterruptedException e1) {
+        //     e1.printStackTrace();
+        // }
+
     }
 }
 
