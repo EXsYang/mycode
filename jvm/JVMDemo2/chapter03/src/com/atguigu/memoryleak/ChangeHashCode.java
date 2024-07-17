ac1e13ee3c5d3e6042572991c0bb377484ca032f
EXsYang <yangda27@aliyun.com>
2024-07-17 23:15:52 +0800

    commit on 2024/07/17 周三

--- jvm/JVMDemo2/chapter03/src/com/atguigu/memoryleak/ChangeHashCode.java
+++ jvm/JVMDemo2/chapter03/src/com/atguigu/memoryleak/ChangeHashCode.java
@@ -4,28 +4,37 @@ import java.util.HashSet;
 
 /**
  * 演示内存泄漏
- *
+ * 演示因修改影响hashCode的字段导致的内存泄漏。
  * @author shkstart
  * @create 14:43
  */
 public class ChangeHashCode {
     public static void main(String[] args) {
         HashSet set = new HashSet();
-        Person p1 = new Person(1001, "AA");
-        Person p2 = new Person(1002, "BB");
+        Person p1 = new Person(1001, "AA"); //0x11
+        Person p2 = new Person(1002, "BB"); //0x22
 
         set.add(p1);
         set.add(p2);
 
-        p1.name = "CC";//导致了内存的泄漏
-        set.remove(p1); //删除失败
+        p1.name = "CC";//这里是修改了name属性值, 导致了内存的泄漏。
+        set.remove(p1); // 0x33 //删除失败,当执行 set.remove(p1); 时，
+        // 才会更新p1方法的hashCode,
+        // HashSet 将根据 p1 的当前(根据新的属性"CC"更新后的)哈希码
+        // （此时已根据 "CC" 计算）来定位 p1
+        // 应该位于的存储位置。由于这个位置并没有与 p1 相匹配的对象
+        // （因为根据旧的hashCode存储的位置和当前新生成的hashCode不同），
+        // 所以 remove 方法找不到要删除的对象，导致删除失败。
 
         System.out.println(set);
 
-        set.add(new Person(1001, "CC"));
+        set.add(new Person(1001, "CC")); //0x33 这里和之前的p1对象的存储地址不同。可以添加进去。
         System.out.println(set);
 
-        set.add(new Person(1001, "AA"));
+        set.add(new Person(1001, "AA")); //0x11 这里和之前的p1对象的存储地址相同,
+        // 但是HashCode值不同,p1由于更新name为"CC"导致更新hashCode=0x33,
+        // 但是p1对象还是存储在hashcode=0x11时计算得来的数组的位置上,由于p1当前的属性更新为了"CC",hashCode=0x33,
+        // 和这里新添加的匿名对象Person对象做equals比较是不同的对象，属于两个不同的对象。可以添加进去。【当然hashcode值也是不同的】
         System.out.println(set);
 
     }
--- jvm/JVMDemo2/chapter03/src/com/atguigu/memoryleak/ChangeHashCode1.java
+++ jvm/JVMDemo2/chapter03/src/com/atguigu/memoryleak/ChangeHashCode1.java
@@ -14,10 +14,19 @@ public class ChangeHashCode1 {
         cc.setX(10);//hashCode = 41
         hs.add(cc);
 
-        cc.setX(20);//hashCode = 51  此行为导致了内存的泄漏
+        cc.setX(20);//hashCode = 51 ,这里是直接修改了hash值, 此行为导致了内存的泄漏
+        // cc.setX(20); 并不会直接导致hash值改变，
+        // 而是下面的 hs.remove(cc);方法时才会直接修改了hash值,
+        // 因为cc的hash值是基于Point的x属性的。
+        // 此时 cc 的 hashCode 应更新为 51，
+        // 但这种变化实际上在 hs.remove(cc) 调用时才被考虑，
+        // 因为 HashSet 在尝试删除时会重新计算 hashCode。
 
-        System.out.println("hs.remove = " + hs.remove(cc));//false
-        hs.add(cc);
+        System.out.println("hs.remove = " + hs.remove(cc));//false ,
+        // remove()方法会在底层调用cc对象的hashCode()方法,导致cc对象生成新的hashCode值,
+        // 与之前的HashCode值不一样了，因此删除失败。
+        // 删除失败，因为 HashSet 正在查找 hashCode 为 51 的元素，而在添加时 cc 的 hashCode 是 41。因此，HashSet 中没有元素匹配当前的 hashCode。
+        hs.add(cc); //尽管 cc 的实例已经在集合中，但由于其 hashCode 已经改变，HashSet 将其视为不同的元素，所以再次添加成功。
         System.out.println("hs.size = " + hs.size());//size = 2
 
         System.out.println(hs);
