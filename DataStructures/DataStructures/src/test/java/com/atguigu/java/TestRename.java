4820ca55aa4d071023da360ba0d667bf8e425cdd
EXsYang <yangda27@aliyun.com>
2025-01-04 17:34:13 +0800

    自动提交 #55: 周六 2025/01/04 17:33:54.76

--- DataStructures/DataStructures/src/test/java/com/atguigu/java/TestRename.java
+++ DataStructures/DataStructures/src/test/java/com/atguigu/java/TestRename.java
@@ -16,52 +16,32 @@ public class TestRename {
 
 
     public static void main(String[] args) {
-        String directoryPath = "D:\\video_production\\命运石之门\\命运石之门4K 58G\\命运石之门 0";
+        String directoryPath = "D:\\Google_Drive\\动漫\\命运石之门\\命运石之门4K 67G";
         File directory = new File(directoryPath);
 
         if (!directory.exists() || !directory.isDirectory()) {
-            System.err.println("指定的目录不存在或不是一个目录: " + directoryPath);
+            System.err.println("目录未找到: " + directoryPath);
             return;
         }
 
-        File[] mkvFiles = directory.listFiles(pathname -> pathname.getName().endsWith(".mkv") && pathname.getName().startsWith("[Bcat] Steins;Gate 0 ["));
-        File[] assFiles = directory.listFiles(pathname -> pathname.getName().endsWith(".ass") && pathname.getName().startsWith("[KissSub][Steins;Gate0]"));
-
-        if (mkvFiles == null || assFiles == null) {
-            System.err.println("读取文件列表失败。");
+        File[] files = directory.listFiles();
+        if (files == null) {
+            System.err.println("无法列出目录中的文件: " + directoryPath);
             return;
         }
 
-        for (File mkvFile : mkvFiles) {
-            String mkvFileName = mkvFile.getName();
-            Pattern pattern = Pattern.compile("\\[Bcat\\] Steins;Gate 0 \\[([0-9]+)\\]");
-            Matcher matcher = pattern.matcher(mkvFileName);
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".mkv")) {
+                String oldName = file.getName();
+                String newName = oldName.substring(0, oldName.length() - 4) + ".ass"; // 将 .mkv 替换为 .ass
+                File newFile = new File(directory, newName);
 
-            if (matcher.find()) {
-                String episodeNumber = matcher.group(1);
-                String paddedEpisodeNumber = String.format("%02d", Integer.parseInt(episodeNumber));
-
-                for (File assFile : assFiles) {
-                    String assFileName = assFile.getName();
-                    String expectedMkvPrefix = "[KissSub][Steins;Gate0][" + paddedEpisodeNumber + "]";
-                    if (assFileName.startsWith(expectedMkvPrefix)) {
-                        String newMkvFileName = assFileName.replace(".ass", ".mkv");
-                        File newMkvFile = new File(directory, newMkvFileName);
-
-                        try {
-                            Files.move(mkvFile.toPath(), newMkvFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                            System.out.println("将 '" + mkvFileName + "' 重命名为 '" + newMkvFileName + "'");
-                            break; // 找到匹配的字幕文件后，跳出字幕文件循环
-                        } catch (IOException e) {
-                            System.err.println("重命名文件 '" + mkvFileName + "' 时出错: " + e.getMessage());
-                        }
-                    }
+                if (file.renameTo(newFile)) {
+                    System.out.println("已将 '" + oldName + "' 重命名为 '" + newName + "'");
+                } else {
+                    System.err.println("重命名 '" + oldName + "' 为 '" + newName + "' 失败");
                 }
-            } else {
-                System.out.println("无法从文件名中提取剧集编号: " + mkvFileName);
             }
         }
-
-        System.out.println("重命名完成。");
     }
 }
