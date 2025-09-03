3bc941fbaaeb73d56634a29d63d0f7b31ee5905a
EXsYang <yangda27@aliyun.com>
2025-09-03 17:34:14 +0800

    自动提交 #127: 周三 2025/09/03 17:33:54.71

---
+++ tingshu_xcx/mp_demo/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,9 @@
+package com.atguigu;/**
+    @create ${YEAR}-${MONTH}-${DAY}-${TIME}
+    @author yangda
+    @description:
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
