e33b3df862c38c4e50e9c3ffd878134c772fb4c3
EXsYang <yangda27@aliyun.com>
2025-09-05 17:34:17 +0800

    自动提交 #129: 周五 2025/09/05 17:33:54.73

--- javase/JavaSenior2/day1/src/com/atguigu/exer/NumberTest.java
+++ javase/JavaSenior2/day1/src/com/atguigu/exer/NumberTest.java
@@ -6,7 +6,13 @@ package com.atguigu.exer;
  * @create 2022-11-08-15:49
  */
 public class NumberTest {
-    //线程通信；两个线程交替打印1~100
+    //线程通信；两个线程交替打印1~100   【下面这段代码存在虚假唤醒问题。】
+
+    /**
+     * 是否存在虚假唤醒问题？ 是。因为它没有遵循将 wait() 放入 while 条件检查循环的标准范式，无法正确处理虚假唤醒。
+     * 代码健壮吗？ 不健壮。它的正确性依赖于两个线程间非常理想化的执行时序，这在复杂的真实环境中是不可靠的。
+     * 如何修正？ 必须引入一个状态变量来判断轮到哪个线程执行，并把 wait() 放入 while 循环中检查这个状态变量。例如，可以使用 number % 2 来判断，或者引入一个 boolean 标志位。
+     */
     public static void main(String[] args) {
         Number number = new Number();
 
--- juc/juc_atguigu/src/com/atguigu/sync/ThreadDemo1.java
+++ juc/juc_atguigu/src/com/atguigu/sync/ThreadDemo1.java
@@ -6,7 +6,29 @@ class Share {
     private int number = 0;
     //+1的方法
     public synchronized void incr() throws InterruptedException {
+
+
         //第二步 判断 干活 通知
+        // if(number != 0) { //判断number值是否是0，如果不是0，等待
+
+        //
+        // 关键问题点：现在轮到 DD 抢到锁了。DD 也从 wait() 的地方醒来。同样，因为它使用的是 if，它也不会再次检查条件，直接执行 number--。但此时 number 已经是 0 了！执行减法后，number 变成了 -1
+        // 通俗的讲，DD wait后释放锁，后来又被DD抢到了锁，但是这时候如果是if判断，则会在睡着的地方醒来，不会走上面的if判断。
+        //     this.wait(); //在哪里睡，就在哪里醒,这里如果使用if判断则会存在虚假唤醒问题。
+        // }
+
+        // API文档描述：
+        // 对于某一个参数的版本，实现中断和虚假唤醒是可能的，而且此方法应始终在循环中使用：
+ //        synchronized (obj) {
+ //            while (<condition does not hold>)
+ //            obj.wait();
+ // Perform action appropriate to condition
+ //        }
+        // 此方法只应由作为此对象监视器的所有者的线程来调用。
+
+
+        // 关键区别：因为使用的是 while 循环，DD 醒来后会重新回到循环的开始，
+        // 再次检查条件 number != 1。
         while(number != 0) { //判断number值是否是0，如果不是0，等待
             this.wait(); //在哪里睡，就在哪里醒
         }
